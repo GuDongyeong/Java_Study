927ccb4a1250abc8d2e533b9f9b7a8d941f82793
GuDongyeong <62394108+GuDongyeong@users.noreply.github.com>
2020-05-10 10:38:23 +0900

    Create Child.java

---
+++ JavaBasic/java08_abstract/interfaceEx/Child.java
@@ -0,0 +1,14 @@
+package java08_abstract.interfaceEx;
+
+
+public class Child extends Parent implements Inter_01, Inter_02 {
+	
+	@Override
+	public void out() {
+		System.out.println("자식클래스");
+	}
+	
+	
+	
+
+}
